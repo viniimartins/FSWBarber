94c6f363fbbbd80ed8ba58e475607ba791803fe3
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-02-01 21:32:21 -0300

    feat: add wecolme message

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,19 @@
+import { format } from "date-fns";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale/pt-BR";
 
 export default function Home() {
     return (
-        <Header />
+        <div>
+            <Header />
+
+
+            <div className="px-5 pt-5">
+                <h1 className="text-xl font-bold">Olá, Miguel!</h1>
+                <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+                    locale: ptBR
+                })}</p>
+            </div>
+        </div>
     );
-  }
-  
+}
