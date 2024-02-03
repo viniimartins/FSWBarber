75e1f8af5cb3a309a653681ffd3bb4866badf986
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-02-03 00:05:38 -0300

    feat: ensure unauthenticated user is redirected to login booking click

--- src/app/barbershops/[id]/_components/barbershop-info.tsx
+++ src/app/barbershops/[id]/_components/barbershop-info.tsx
@@ -17,7 +17,7 @@ const BarbershopInfo = ({ barbershop }: BarbershopDatailsPageProps) => {
     const router = useRouter()
 
     const handleBackClick = () => {
-        router.back()
+        router.replace('/')
     }
 
     return (
--- src/app/barbershops/[id]/page.tsx
+++ src/app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDatailsPageProps {
     params: {
@@ -9,6 +11,8 @@ interface BarbershopDatailsPageProps {
 }
 
 const BarbershopDatailsPage = async ({ params }: BarbershopDatailsPageProps) => {
+    const session = await getServerSession(authOptions)
+
     if (!params.id) {
         return null
     }
@@ -32,7 +36,7 @@ const BarbershopDatailsPage = async ({ params }: BarbershopDatailsPageProps) => 
 
             <div className="px-5 flex flex-col gap-4 py-6">
                 {barbershop.service.map((service) => (
-                    <ServiceItem key={service.id} service={service} />
+                    <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
                 ))}
             </div>
 
